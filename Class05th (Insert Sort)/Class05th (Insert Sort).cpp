8252faf5d54fef8c9cedcc730cfb9b65e395e283
JeongAaron <lovejsk1004@gmail.com>
2025-03-17 19:39:40 +0900

    2025-03-17

---
+++ Class05th (Insert Sort)/Class05th (Insert Sort).cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+#define SIZE 5
+
+using namespace std;
+
+int main()
+{
+#pragma region 삽입 정렬
+
+	//	데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여 자신의 위치를 찾아 삽입하여 정렬하는 알고리즘
+
+	int array[SIZE] = { 9,6,8,1,3 };
+	int key = 0;
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		key = array[i + 1];
+		for (int j = 0; j < i + 1; j++)
+		{
+			if (array[i - j] > array[i - j + 1])
+			{
+				array[i - j + 1] = array[i - j];
+				array[i - j] = key;
+			}
+		}
+	}
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl;
+	for (const int& element : array)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+}
---
+++ Class06th (Euclidean)/Class06th (Euclidean).cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int euclid(int x, int y)
+{
+	while (y != 0)
+	{
+		int R = x % y;
+		if (R == 0)
+		{
+			return y;
+		}
+		else
+		{
+			x = y;
+			y = R;
+		}
+	}
+}
+
+int main()
+{
+#pragma region 유클리드 호제법
+
+	//	2개의 자연수 또는 정식의 최대 공약수를 구하는 방식으로 두 수가
+	//	서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘
+
+	int a = 155;
+	int b = 65;
+	int result = 0;
+	for (int i = 1; i <= a && i <= b; i++)
+	{
+		if (a % i == 0 && b % i == 0)
+		{
+			result = i;
+		}
+	}
+
+	cout << "최대 공약수 : " << result << " ";
+	cout << endl;
+	cout << "최대 공약수 : " << euclid(a, b) << " ";
+
+#pragma endregion
+
+}
