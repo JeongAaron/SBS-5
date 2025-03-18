865bc5d6edceb1dd718dc27343d9bfd269f9b703
JeongAaron <lovejsk1004@gmail.com>
2025-03-18 19:44:09 +0900

    2025-03-18

---
+++ Class07th (Sieve of Eratosthenes)/Class07th (Sieve of Eratosthenes).cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool isPrime(int n)
+{
+	int count = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			count++;
+		}
+	}
+	if (count == 2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool isPrime1(int n)
+{
+	if (n <= 1)
+	{
+		return false;
+	}
+	for (int i = 2; i < n; i++)
+	{
+		if (n % 2 == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void sieve(int n)
+{
+	int* array = new int[n + 1];
+
+	for (int i = 0; i <= n; i++)
+	{
+		array[i] = i;
+	}
+	for (int i = 2; i <= sqrt(n); i++)
+	{
+		if (array[i] == 0)
+		{
+			continue;
+		}
+		for (int j = i * 2; j <= n; j += i)
+		{
+			array[j] = 0;
+		}
+	}
+	for (int i = 2; i <= n; i++)
+	{
+		if (array[i] != 0)
+		{
+			cout << array[i] << " ";
+		}
+	}
+	delete[] array;
+}
+int main()
+{
+	for (int i = 0; i < 30; i++)
+	{
+		cout << boolalpha << i << " is Prime Number " << isPrime(i) << endl;
+	}
+	sieve(16);
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 int main()
 {
+#pragma region 계수 정렬
+
+	//	데이터의 값을 비교하지 않고 각 원소에 데이터가 몇 개 있는 지
+	//	개수를 세어 저장한 다음 정렬하는 알고리즘
+
+    int array[] = { 1, 1, 3, 1, 2, 3, 4, 4, 2, 5 };
+    int list[5] = { 0 };
+
+
+    for (int i = 0; i < 10; i++) 
+	{
+		int x = array[i] % 5;
+        list[x]++;
+    }
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << list[i] << " ";
+	}
+#pragma endregion
 
 }
