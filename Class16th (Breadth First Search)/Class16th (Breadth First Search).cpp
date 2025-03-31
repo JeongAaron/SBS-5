20b0ccfd0e7ac5cbdbc77dc7f1019ee257c38b10
JeongAaron <lovejsk1004@gmail.com>
2025-03-31 19:42:53 +0900

    2025-03-31

---
+++ Class16th (Breadth First Search)/Class16th (Breadth First Search).cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+#include <vector>
+
+#include <queue>
+
+#define SIZE 8
+
+using namespace std;
+
+class Node
+{
+private:
+	bool visited[SIZE];
+	vector<int> adjacencyList[SIZE];
+	queue<int> queue;
+public:
+	Node()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			visited[i] = false;
+		}
+	}
+	void insert(int i, int j)
+	{
+		adjacencyList[i].push_back(j);
+		adjacencyList[j].push_back(i);
+	}
+	void search(int start)
+	{
+		visited[start] = true;
+		queue.push(start);
+		while (queue.empty() == false)
+		{
+			int x = queue.front();
+			queue.pop();
+			cout << x << " ";
+			for (int i = 0;i < adjacencyList[x].size();i++)
+			{
+				int next = adjacencyList[x][i];
+				if (visited[next] == false)
+				{
+					queue.push(next);
+					visited[next] = true;
+				}
+			}
+		}
+	}
+};
+int main()
+{
+#pragma region 너비 우선 탐색(Breadth First Search)
+
+	//	시작 정점을 방문한 후 시작 정점에 인접한 모든 정점들을 우선 방문하는 방법
+
+	//	더 이상 방문하지 않은 정점이 없을 때까지 방문하지 않은 모든 정점들에 대해서도 너비 우선 탐색 적용
+
+	Node graph;
+	graph.insert(1, 2);
+	graph.insert(1, 3);
+	graph.insert(2, 4);
+	graph.insert(2, 5);
+	graph.insert(3, 6);
+	graph.insert(3, 7);
+	graph.search(2);
+
+#pragma endregion
+
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,70 +1,88 @@
 ﻿#include <iostream>
 
-#include <vector>
-
 #include <queue>
+
+#include <vector>
 
 #define SIZE 8
 
 using namespace std;
 
-class Node
+class Graph
 {
 private:
-	bool visited[SIZE];
-	vector<int> adjacencyList[SIZE];
 	queue<int> queue;
+	vector<int> adjacencyList[SIZE];
+	int degree[SIZE];
 public:
-	Node()
+	Graph()
 	{
-		for (int i = 0; i < SIZE; i++)
+		for (int i = 0; i < SIZE;i++)
 		{
-			visited[i] = false;
+			degree[i] = 0;
 		}
 	}
-	void insert(int i, int j)
+	void insert(int vertex, int edge)
 	{
-		adjacencyList[i].push_back(j);
-		adjacencyList[j].push_back(i);
+		adjacencyList[vertex].push_back(edge);
+		degree[edge]++;
 	}
-	void search(int start)
+	void sort()
 	{
-		visited[start] = true;
-		queue.push(start);
+		for (int i = 1;i < SIZE; i++)
+		{
+			if (degree[i] == 0)
+			{
+				queue.push(i);
+			}
+		}
 		while (queue.empty() == false)
 		{
 			int x = queue.front();
 			queue.pop();
 			cout << x << " ";
-			for (int i = 0;i < adjacencyList[x].size();i++)
+			for (int i = 0; i < adjacencyList[x].size();i++)
 			{
 				int next = adjacencyList[x][i];
-				if (visited[next] == false)
+				degree[next]--;
+				if (degree[next] == 0)
 				{
 					queue.push(next);
-					visited[next] = true;
 				}
+				
 			}
 		}
 	}
+
 };
 int main()
 {
-#pragma region 너비 우선 탐색(Breadth First Search)
+#pragma region 위상 정렬
+	
+	//	병합 그래프에 존재하는 각 정점들의 선행 순서를 지키며,
+	//	모든 정점을 차례대로 진행하는 알고리즘
 
-	//	시작 정점을 방문한 후 시작 정점에 인접한 모든 정점들을 우선 방문하는 방법
+	//	사이클이 발생하는 경우 위상 절렬을 수행할 수 없다.
 
-	//	더 이상 방문하지 않은 정점이 없을 때까지 방문하지 않은 모든 정점들에 대해서도 너비 우선 탐색 적용
+	//	DAG(Directed Acyclic Graph) : 사이클이 존재하지 않는 그래프
+	
+	//	시간 복잡도 : O(V + E)
 
-	Node graph;
+	// 위상 정렬 방법
+	// 1. 진입 차수가 0인 정점을 Queue에 삽입
+	// 2. Queue에서 원소를 꺼내 연결된 모든 간선을 제거
+	// 3. 간선 제거 이후에 진입 차수가 0이 된 정점을 Queue에 삽입
+	// 4. Queue가 비어있을 때까지 2~3번 작업을 반복 수행
+
+	Graph graph;
 	graph.insert(1, 2);
-	graph.insert(1, 3);
-	graph.insert(2, 4);
-	graph.insert(2, 5);
-	graph.insert(3, 6);
-	graph.insert(3, 7);
-	graph.search(2);
-
+	graph.insert(1, 5);
+	graph.insert(2, 3);
+	graph.insert(3, 4);
+	graph.insert(4, 6);
+	graph.insert(5, 6);
+	graph.insert(6, 7);
+	graph.sort();
 #pragma endregion
 
 }
