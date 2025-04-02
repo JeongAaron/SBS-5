7eb048cb68d12ca02ca6b3993089eed0a1db7be1
JeongAaron <lovejsk1004@gmail.com>
2025-04-02 19:41:39 +0900

    2025-04-02

---
+++ Class18th (Union Find)/Class18th (Union Find).cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+#define SIZE 6
+
+int list[SIZE];
+
+using namespace std;
+
+int find(int x)
+{
+	if (x == list[x])
+	{
+		return x;
+	}
+	else
+	{
+		return list[x] = find(list[x]);
+	}
+}
+
+void Union(int x, int y)
+{
+	x = find(x);
+	y = find(y);
+	if (x == y)
+	{
+		return;
+	}
+	if (x < y)
+	{
+		list[y] = x;
+	}
+	else
+	{
+		list[x] = y;
+	}
+}
+
+bool same(int x, int y)
+{
+	return find(x) == find(y);
+}
+
+int main()
+{
+#pragma region 유니온 파인드
+
+	//	여러 노드가 존재할 때 어떤 노드가 다른 노드와 연결되어 있는 지
+	//	확인하는 알고리즘
+
+	//	Union : 특정한 두 개의 노드를 같은 집합으로 합치는 연산
+
+	//	Find : 특정한 노드가 어느 집합에 있었는 지 확인하는 연산
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		list[i] = i;
+	}
+	Union(0, 1);
+	Union(3, 4);
+	Union(4, 5);
+
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		cout << same(i, i + 1) << " ";
+	}
+
+#pragma endregion
+
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,74 +1,66 @@
 ﻿#include <iostream>
 
-#define SIZE 6
+#include <vector>
 
-int list[SIZE];
+#define SIZE 8
 
 using namespace std;
 
-int find(int x)
+class Kruskal
 {
-	if (x == list[x])
+private:
+	class Edge
 	{
-		return x;
-	}
-	else
+	private :
+		int vertexX, vertexY, weight;
+	public:
+		Edge(int vertexX, int vertexY, int weight)
+		{
+			this->vertexX = vertexX;
+			this->vertexY = vertexY;
+			this->weight = weight;
+		}
+	};
+	int cost;
+	int parent[SIZE];
+	vector<Edge> nodeList;
+public:
+	Kruskal()
 	{
-		return list[x] = find(list[x]);
+		cost = 0;
+		for (int i = 0;i < SIZE;i++)
+		{
+			parent[i] = i;
+		}
 	}
-}
+	void insert(int x, int y, int z)
+	{
+		nodeList.push_back(Edge(x, y, z));
+	}
 
-void Union(int x, int y)
-{
-	x = find(x);
-	y = find(y);
-	if (x == y)
-	{
-		return;
-	}
-	if (x < y)
-	{
-		list[y] = x;
-	}
-	else
-	{
-		list[x] = y;
-	}
-}
-
-bool same(int x, int y)
-{
-	if (find(x) == find(y))
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-
+};
 int main()
 {
-#pragma region 유니온 파인드
+#pragma region 최소 신장 트리
 
-	//	여러 노드가 존재할 때 어떤 노드가 다른 노드와 연결되어 있는 지
-	//	확인하는 알고리즘
+	//	그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는 부분 그래프로,
+	//	그래프의 모든 정점을 최소 비용으로 연결하는 트리
 
-	//	Union : 특정한 두 개의 노드를 같은 집합으로 합치는 연산
+	//	그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개
 
-	//	Find : 특정한 노드가 어느 집합에 있었는 지 확인하는 연산
+	Kruskal kruskal;
+	kruskal.insert(1, 2, 64);
+	kruskal.insert(1, 4, 30);
+	kruskal.insert(1, 5, 19);
+	kruskal.insert(1, 7, 10);
+	kruskal.insert(2, 4, 25);
+	kruskal.insert(2, 5, 61);
+	kruskal.insert(3, 5, 22);
+	kruskal.insert(3, 6, 36);
+	kruskal.insert(4, 7, 14);
+	kruskal.insert(5, 6, 48);
+	kruskal.insert(5, 7, 73);
 
-	for (int i = 0; i < SIZE; i++)
-	{
-		list[i] = i;
-	}
-	Union(0, 1);
-	Union(3, 4);
-	Union(4, 5);
-
-	cout << find(5) << " ";
-	
 #pragma endregion
 
 }
